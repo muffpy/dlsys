92aa52a7586c4eaca6686e1b73375d3856df720f
muffpy <rahulathreya@yahoo.com>
2024-07-18 11:00:32 +0100

    autograd tests

--- tests/test_autograd.py
+++ tests/test_autograd.py
@@ -2,11 +2,11 @@ import sys
 
 sys.path.append("./python")
 sys.path.append("./apps")
+sys.path.append("./src")
 from simple_ml import *
 import numdifftools as nd
 
 import numpy as np
-import mugrade
 import needle as ndl
 
 
